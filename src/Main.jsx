5a5262ff471b1a37e06c70c034fb46501d109653
Mariano Conti <nanexcool@gmail.com>
2019-11-20 17:14:51 -0300

    Fix banner

--- src/Main.jsx
+++ src/Main.jsx
@@ -15,11 +15,8 @@ const Main = (props) => {
   document.title = `Dai Stats - ${formatAmount.format(props.debt)}`
   return (
     <div>
-      <div className="notification is-primary">
-        <button className="delete"></button>
-        This page updates automatically every {props.interval} seconds.
-        adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
-        consectetur adipiscing elit
+      <div className="notification is-primary has-text-centered">
+        This page updates automatically every {props.interval / 1000} seconds. Give Infura a break :D
       </div>
       <section className="section">
         <div className="container">
