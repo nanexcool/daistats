68e33f019a656a8aae08ed43769e06358fabfbaa
Mariano Conti <nanexcool@gmail.com>
2020-01-29 16:30:38 -0300

    Merge

--- src/components/MeetingTime.jsx
+++ src/components/MeetingTime.jsx
@@ -15,28 +15,28 @@ var riskTitle = "MakerDAO Governance and Risk Meeting"
 var riskUrl = "https://zoom.us/j/697074715"
 
 function MeetingNote(props) {
-    if (props.show) {
-        return (
-            <div className="columns">
-                <div className="column">
-                    <div className="box has-text-centered">
-                        <h4 className="subtitle is-size-4">{props.title} is happening now!</h4>
-                        <h4><a href={props.url}>{props.url}</a></h4>
-                    </div>
-                </div>
-            </div>
-        )
-    } else {
-        return null;
-    }
+  if (props.show) {
+    return (
+      <div className="columns">
+        <div className="column">
+          <div className="box has-text-centered">
+            <h4 className="subtitle is-size-4">{props.title} is happening now!</h4>
+            <h4><a href={props.url}>{props.url}</a></h4>
+          </div>
+        </div>
+      </div>
+    )
+  } else {
+    return null;
+  }
 }
 
 function MeetingTime(props) {
 
   return (
-      <React.Fragment>
-        <MeetingNote show={communityMeeting} title={communityTitle} url={communityUrl} />
-        <MeetingNote show={riskMeeting} title={riskTitle} url={riskUrl} />
-      </React.Fragment>
+    <React.Fragment>
+      <MeetingNote show={communityMeeting} title={communityTitle} url={communityUrl} />
+      <MeetingNote show={riskMeeting} title={riskTitle} url={riskUrl} />
+    </React.Fragment>
   );
 }
