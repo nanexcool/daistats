f49c81f173535b663bc90472ec4cc6a18394a5e3
Brian L. McMichael <brian@brianmcmichael.com>
2020-02-07 14:42:32 -0500

    Fix smiley react warning (#44)

--- src/Main.jsx
+++ src/Main.jsx
@@ -265,7 +265,7 @@ const Main = (props) => {
             </div>
             <div className="column">
               <div className="box has-text-centered">
-                <h3 className="title" title={props.flapKicks}>{formatAmount.format(props.flapKicks)} 😃</h3>
+                <h3 className="title" title={props.flapKicks}>{formatAmount.format(props.flapKicks)} <span role="img" aria-label="smiley">😃</span></h3>
                 <p className="title subtitle is-size-4">Dai Surplus (Flap) Auctions</p>
                 <p className="subtitle is-size-6">Till next Flap: {formatAmount.format((Number(props.surplusBuffer) + Number(props.surplusBump) + Number(props.sysDebt) + Number(props.sin)) - Number(props.sysSurplus))}</p>
               </div>
