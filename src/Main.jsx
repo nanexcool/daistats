86b3107f90a45a04ba997cc7cb9f59764bf6689e
Mariano Conti <nanexcool@gmail.com>
2020-03-18 14:04:41 -0300

    typo

--- src/Main.jsx
+++ src/Main.jsx
@@ -88,7 +88,7 @@ const Main = (props) => {
           <div className="columns">
             <div className="column">
               <div className="box has-text-centered">
-                <h4 className="subtitle is-size-4">MKR Debt Auction start Monday March 19 around 10:28 ET (14:28 UTC)</h4>
+                <h4 className="subtitle is-size-4">MKR Debt Auction start Thursday March 19 around 10:28 ET (14:28 UTC)</h4>
                 <h4>
                   <a href="https://blog.makerdao.com/mkr-debt-auction-announcement-and-details/" target="_blank" rel="noopener noreferrer">
                     Read more about it on the Maker Blog
