f483d06643c875c6c70542afbcd438a8b68daa37
Mariano Conti <nanexcool@gmail.com>
2020-03-27 13:12:35 -0300

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -88,10 +88,10 @@ const Main = (props) => {
           <div className="columns">
             <div className="column">
               <div className="box has-text-centered">
-                <h4 className="subtitle is-size-4">MKR Debt Auction start Thursday March 19 around 10:28 ET (14:28 UTC)</h4>
+                <h4 className="subtitle is-size-4">Last batch of MKR Auctions are live</h4>
                 <h4>
-                  <a href="https://blog.makerdao.com/mkr-debt-auction-announcement-and-details/" target="_blank" rel="noopener noreferrer">
-                    Read more about it on the Maker Blog
+                  <a href="https://auctions.makerdao.com/flop" target="_blank" rel="noopener noreferrer">
+                    Follow or bid on auctions here
                   </a>
                 </h4>
               </div>
