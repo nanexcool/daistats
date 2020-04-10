776cf49f40d1ab3c281dde482452c7175779873b
pacxiu <nedvedek111@gmail.com>
2020-04-10 11:21:38 +0200

    Add DAI balance for Oasis Dex, hide MKR in Uniswap

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,6 @@ const utils = ethers.utils
 const jsonFetch = url => fetch(url).then(res => res.json())
 
 const add = require('./addresses.json')
-add["GEM_PIT"] = "0x69076e44a9C70a67D5b79d95795Aba299083c275"
-add["UNISWAP_DAI"] = "0x2a1530C4C41db0B0b2bB646CB5Eb1A67b7158667"
-add["UNISWAP_MKR"] = "0x2C4Bd064b998838076fa341A83d007FC2FA50957"
-add["MULTICALL"] = "0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441"
-add["CHAI"] = "0x06AF07097C9Eeb7fD685c692751D5C66dB49c215"
 
 let provider;
 let networkId;
@@ -163,6 +158,7 @@ class App extends Component {
       [add.MCD_VOW, vow.interface.encodeFunctionData('dump', [])],
       [add.PIP_USDC, usdcPip.interface.encodeFunctionData('read', [])],
       [add.MCD_GOV, mkr.interface.encodeFunctionData('balanceOf', [add.UNISWAP_MKR])],
+      [add.MCD_DAI, dai.interface.encodeFunctionData('balanceOf', [add.OASIS_DEX])],
     ], {blockTag: blockNumber})
     let p2 = this.etherscanEthSupply()
     let p3 = this.getOSMPrice(add.PIP_ETH, this.POSITION_NXT)
@@ -226,7 +222,8 @@ class App extends Component {
     const usdcSupply = usdc.interface.decodeFunctionResult('totalSupply', res[42])
     const usdcLocked = usdc.interface.decodeFunctionResult('balanceOf', res[43])
     const usdcPrice = usdcPip.interface.decodeFunctionResult('read', res[46])[0]
-    const scdFee = saiTubTax + saiTubFee
+    const scdFee = saiTubTax + saiTubFee;
+    const oasisDexDai = dai.interface.decodeFunctionResult('balanceOf', res[48]);
     this.setState(state => {
       return {
         networkId: networkId,
@@ -316,7 +313,8 @@ class App extends Component {
         vow_sin: utils.formatUnits(vow_sin[0], 45),
         bigSin: utils.formatUnits(sin[0], 45),
         daiBrewing: utils.formatUnits(daiBrewing, 45),
-        darkMode: JSON.parse(localStorage.getItem("ds-darkmode"))
+        darkMode: JSON.parse(localStorage.getItem("ds-darkmode")),
+        oasisDexDai: utils.formatEther(oasisDexDai[0]),
       }
     })
       // confetti.rain()
