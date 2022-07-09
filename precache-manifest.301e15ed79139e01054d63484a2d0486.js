333af9a9d8d7df3d1cfa889be7d72078b9c83798
devenoges <github@devenoges.com>
2022-07-09 12:23:55 +1200

    Updates

--- precache-manifest.301e15ed79139e01054d63484a2d0486.js
+++ precache-manifest.301e15ed79139e01054d63484a2d0486.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "15a031f3e0de57eca4c6d54d786b9c15",
+    "revision": "d6cd0231c63969b32e93d05c746e3b2a",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.5c17b9f1.chunk.css"
   },
   {
-    "revision": "7dfcc2a78dab47fbcf93",
+    "revision": "361b38cb5b441d6820ca",
     "url": "/static/css/main.7bfd0464.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.518d5e01.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7dfcc2a78dab47fbcf93",
-    "url": "/static/js/main.3cf8655c.chunk.js"
+    "revision": "361b38cb5b441d6820ca",
+    "url": "/static/js/main.fc068403.chunk.js"
   },
   {
     "revision": "2c9a9a5880b6b6d1348e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea7d35c8dbff343382abf4435e8d25d6.js"
+  "/precache-manifest.301e15ed79139e01054d63484a2d0486.js"
 );
 
 self.addEventListener('message', (event) => {
